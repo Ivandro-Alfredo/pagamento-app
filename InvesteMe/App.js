ca71aa2be416001142430bcbc980c3da6defaf6b
Ivandro Alfredo <ivandroalfredo10@gmail.com>
2023-12-11 05:46:27 +0100

    fix: solve problem with the path.

--- InvesteMe/App.js
+++ InvesteMe/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Cadastro from './src/screnens/Cadastro/Cadastro';
+import Cadastro from './src/screens/Cadastro/Cadastro';
 
 export default function App() {
   return (
